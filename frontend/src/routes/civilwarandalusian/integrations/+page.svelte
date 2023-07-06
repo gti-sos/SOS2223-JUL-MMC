d4a78fb85e725641df7a7fa00cf2882bf7b4fd7b
Marcos <marcosmorales1812@gmail.com>
2023-07-06 20:58:53 +0200

    Integraciones5

--- frontend/src/routes/civilwarandalusian/integrations/+page.svelte
+++ frontend/src/routes/civilwarandalusian/integrations/+page.svelte
@@ -58,23 +58,33 @@
 
     async function getRapidApi() {
       console.log("API");
+      const options = {
+        method: 'GET',
+        url: "https://sos2223-jul-mmc-1.appspot.com/agroclimatizaciones",
+        headers: {
+          'content-type': 'application/octet-stream',
+          
+          'X-RapidAPI-Key': '031e30be2bmsh4012077306f5d0fp169105jsn72f6d2c3886b',
+		  'X-RapidAPI-Host': 'gas-price.p.rapidapi.com',
+        }
+      };
       try{
-        const pprox = await fetch(API_prox,{
-                method: 'GET',
-                headers: {
-                    'content-type': 'application/octet-stream',
-                    'X-RapidAPI-Host': 'gas-price.p.rapidapi.com',
-                    'X-RapidAPI-Key': '031e30be2bmsh4012077306f5d0fp169105jsn72f6d2c3886b'
-		  
-                        }
-                    });
-            const jsonData = await pprox.json();
+       
+        //const pprox = await fetch(API_prox,{
+               // method: 'GET',
+                
+             //   });
+            const jsonData = await axios.request(options);
            datos2 = jsonData;
            console.log(datos2);
         } catch (error) {
           console.error(error);
         }
-         }
+
+        
+
+
+    }
 
     async function getRapidApi2() {
 
