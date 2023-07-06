efba1b5125928ccf795e4c6f325b734efed95452
Marcos <marcosmorales1812@gmail.com>
2023-07-06 21:22:13 +0200

    Integraciones8

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ console.log("c");
 const PORT = 8080;
 
 
-var ruta = "/agroclimatizaciones";
-//var apiServerHost = "https://sos2223-12.appspot.com/api/v1/agroclimatic";
-let apiServerHost = "https://gas-price.p.rapidapi.com/europeanCountries";
-app.use(ruta, function(req, res) {
-    
+
+app.use(ruta,function(req, res) {
+    var ruta = "/agroclimatizaciones";
+    //var apiServerHost = "https://sos2223-12.appspot.com/api/v1/agroclimatic";
+    let apiServerHost = "https://gas-price.p.rapidapi.com/europeanCountries";
     let url = apiServerHost + req.url;
     console.log('piped: proxy' + req.url);
     console.log(url);
